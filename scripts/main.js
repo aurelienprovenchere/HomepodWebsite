bf8526976918472d16e159b8199a16c21543468a
Aurelien Sarrat <aurelien.sarrat@gmail.com>
2019-02-18 17:06:28 +0100

    Update main.js

--- scripts/main.js
+++ scripts/main.js
@@ -1,1 +1,14 @@
-js
+const sliderImages = document.querySelectorAll('.sliderContainer img')
+const sliderContainer = document.querySelector('.sliderContainer')
+let step = -960,
+    time = 2000,
+    pos = 0
+
+let slide = setInterval(
+  function(){
+    pos = (pos+1)%sliderImages.length
+
+    sliderContainer.style.left = pos*step+'px'
+  },
+  time
+)
